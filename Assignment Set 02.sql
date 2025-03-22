a6c3fad059d6496d273cd1cc5da6f6962d401c3d
Deeps1627 <dkdeepak16896@gmail.com>
2025-03-22 13:47:48 +0530

    Adding new project

---
+++ Assignment Set 02.sql
@@ -0,0 +1,14 @@
+
+-- Using tables from ConsolidatedTables.
+
+-- Q1. select all employees in department 10 whose salary is greater than 3000. [table: employee]
+-- Q2. The grading of students based on the marks they have obtained is done as follows:
+-- 40 to 50 -> third Class
+-- 50 to 60 -> second Class
+-- 60 to 80 -> First Class
+-- 80 to 100 -> Distinctions
+-- a. How many students have graduated with first class?
+-- b. How many students have obtained distinction?
+-- Q3. Get a list of city names from station with even ID numbers only. Exclude duplicates from your answer. [table: station] 
+-- Q4. How many cities in the CITY table have a Population larger than 100000. [table: city]
+-- Q5. What is the total population of California? [table: city]
